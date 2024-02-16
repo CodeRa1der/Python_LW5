de155c9302162ed57516dc965517d1ae4622bd40
Code_Ra1der <kostydu2342@gmail.com>
2024-02-16 20:25:39 +0300

    Minor fixes

--- PyProject/Tasks/Task1.py
+++ PyProject/Tasks/Task1.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-exams = int(input("Сколько экзаменов мы сдали? "))
+if __name__ == '__main__':
+    exams = int(input("Сколько экзаменов мы сдали? "))
 
-if exams > 20:
-    print("Слишком много экзаменов!")
-elif exams == 1:
-    print("Мы успешно сдали 1 экзамен")
-elif exams >= 2 and exams <= 4:
+    if exams > 20:
+        print("Слишком много экзаменов!")
+    elif exams == 1:
+        print("Мы успешно сдали 1 экзамен")
+    elif exams >= 2 and exams <= 4:
     print("Мы успешно сдали", exams, "экзамена")
-elif exams >= 5:
+    elif exams >= 5:
     print("Мы успешно сдали", exams, "экзаменов")
--- PyProject/Tasks/Task3.py
+++ PyProject/Tasks/Task3.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-dec = 9
-count = 0
-for i in range (1, 9):
-    for j in range (0, 9):
-        dec += 1
-        sum = i + j
-        if sum + sum**2 == dec:
-            count += 1
-print("Всего существует", dec, "таких чисел")
+if __name__ == '__main__':
+    dec = 9
+    count = 0
+    for i in range (1, 9):
+        for j in range (0, 9):
+            dec += 1
+            sum = i + j
+            if sum + sum**2 == dec:
+                count += 1
+    print("Всего существует", dec, "таких чисел")
